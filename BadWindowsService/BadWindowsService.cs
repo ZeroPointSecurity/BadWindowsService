67bbca8a58d6553367f6915c0b163d7b0077ca04
Rasta Mouse <rasta@rastamouse.me>
2025-01-02 12:35:12 +0000

    Add unsafe deserialization and toctou

--- BadWindowsService/BadWindowsService.cs
+++ BadWindowsService/BadWindowsService.cs
@@ -2,6 +2,9 @@
 using System.Diagnostics;
 using System.IO;
 using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.AccessControl;
+using System.Security.Principal;
 using System.ServiceProcess;
 using System.Threading;
 
@@ -9,12 +12,6 @@ namespace BadWindowsService;
 
 public partial class BadWindowsService : ServiceBase
 {
-    [DllImport("kernel32", CharSet = CharSet.Ansi)]
-    private static extern IntPtr LoadLibraryA(string lpFileName);
-
-    [DllImport("kernel32", CharSet = CharSet.Ansi)]
-    private static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
-    
     [UnmanagedFunctionPointer(CallingConvention.StdCall)]
     private delegate bool BadFunc();
 
@@ -37,9 +34,16 @@ public partial class BadWindowsService : ServiceBase
 
         _cts = new CancellationTokenSource();
         
-        // run the loop
-        var t = new Thread(DoBadThings);
-        t.Start();
+        // run the loops
+        var t1 = new Thread(LoadDll);
+        var t2 = new Thread(RunExecutable);
+        var t3 = new Thread(DeserializeFile);
+        var t4 = new Thread(Race);
+        
+        t1.Start();
+        t2.Start();
+        t3.Start();
+        t4.Start();
     }
 
     protected override void OnStop()
@@ -47,44 +51,115 @@ public partial class BadWindowsService : ServiceBase
         _cts.Cancel();
     }
 
-    private void DoBadThings()
+    private void LoadDll()
+    {
+        const string moduleName = "BadDll.dll";
+        const string funcName = "BadFunc";
+        
+        while (!_cts.IsCancellationRequested)
+        {
+            try
+            {
+                // load module
+                var hModule = Win32.LoadLibraryA(moduleName);
+                if (hModule == IntPtr.Zero)
+                    throw new DllNotFoundException($"{moduleName} not found.");
+
+                // get func address
+                var hFunc = Win32.GetProcAddress(hModule, funcName);
+                if (hFunc == IntPtr.Zero)
+                    throw new ApplicationException($"{funcName} not found.");
+
+                // marshal function pointer
+                var badFunc = Marshal.GetDelegateForFunctionPointer<BadFunc>(hFunc);
+
+                // execute it
+                if (badFunc() == false)
+                    throw new ApplicationException("Result from BadFunc was false.");
+            }
+            catch
+            {
+                // ignore
+            }
+
+            Thread.Sleep(new TimeSpan(0, 0, 30));
+        }
+    }
+
+    private void RunExecutable()
     {
         while (!_cts.IsCancellationRequested)
         {
             try
             {
-                const string moduleName = "BadDll.dll";
-                const string funcName = "BadFunc";
-                
-                // load module
-                var hModule = LoadLibraryA(moduleName);
-                if (hModule == IntPtr.Zero)
-                    throw new DllNotFoundException($"{moduleName} not found.");
-                
-                // get func address
-                var hFunc = GetProcAddress(hModule, funcName);
-                if (hFunc == IntPtr.Zero)
-                    throw new ApplicationException($"{funcName} not found.");
-                
-                // marshal function pointer
-                var badFunc = Marshal.GetDelegateForFunctionPointer<BadFunc>(hFunc);
-                
-                // execute it
-                if (badFunc() == false)
-                    throw new ApplicationException("Result from BadFunc was false.");
-            }
-            catch { };
-
-            try
-            {
-                // run executable
                 Process.Start("cmd.exe", "/c exit");
             }
-            catch { };
+            catch
+            {
+                // ignore
+            }
+            
+            Thread.Sleep(new TimeSpan(0, 1, 0));
+        }
+    }
 
+    private void DeserializeFile()
+    {
+        const string file = "data.bin";
+        
+        while (!_cts.IsCancellationRequested)
+        {
+            if (File.Exists(file))
+            {
+                try
+                {
+                    var fs = File.OpenRead(file);
+                    var bf = new BinaryFormatter();
+                    _ = bf.UnsafeDeserialize(fs, null);
+                }
+                catch
+                {
+                    // ignore
+                }
+            }
+            
             Thread.Sleep(new TimeSpan(0, 0, 30));
         }
+    }
+
+    private void Race()
+    {
+        const string file = "data.bin";
         
-        _cts.Dispose();
+        while (!_cts.IsCancellationRequested)
+        {
+            try
+            {
+                if (!File.Exists(file))
+                {
+                    // create it
+                    using var fs = File.Create(file);
+                    
+                    // simulate some other work
+                    Thread.Sleep(new TimeSpan(0, 0, 0, 0, 500));
+                    
+                    // set new permissions on file
+                    var rule = new FileSystemAccessRule(
+                        new SecurityIdentifier(WellKnownSidType.AuthenticatedUserSid, null),
+                        FileSystemRights.FullControl, 
+                        AccessControlType.Allow);
+                    
+                    var security = new FileSecurity();
+                    security.AddAccessRule(rule);
+                    File.SetAccessControl(file, security);
+                }
+            }
+            catch
+            {
+                // ignore
+            }
+            
+            Thread.Sleep(new TimeSpan(0, 0, 30));
+        }
     }
 }
---
+++ BadWindowsService/Win32.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace BadWindowsService;
+
+public static class Win32
+{
+    [DllImport("kernel32", CharSet = CharSet.Ansi)]
+    public static extern IntPtr LoadLibraryA(string lpFileName);
+
+    [DllImport("kernel32", CharSet = CharSet.Ansi)]
+    public static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+}
